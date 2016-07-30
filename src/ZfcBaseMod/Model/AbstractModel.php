d016926a0570bfcc0d119b6c8c1bd1d98a275dbb
Voodoo773 <Voodoo773@gmail.com>
2016-07-30 12:49:37 +0300

    add EventProvider to model

--- src/ZfcBaseMod/Model/AbstractModel.php
+++ src/ZfcBaseMod/Model/AbstractModel.php
@@ -5,8 +5,9 @@ use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\ServiceManager\ServiceLocatorAwareTrait;
 use ZfcBaseMod\Model\Aware\SqlAwareTrait;
 use ZfcBaseMod\Model\Aware\SqlAwareInterface;
+use ZfcBase\EventManager\EventProvider;
 
-abstract class AbstractModel 
+abstract class AbstractModel extends EventProvider
     implements SqlAwareInterface, ServiceLocatorAwareInterface
 {
     use SqlAwareTrait;
